2d1081f31115c2aa01f1f8b6572029c35b1757a5
Tom Bagby <tombagby@gmail.com>
2008-08-20 22:45:23 -0700

    Touching things up, starting to add unit tests

---
+++ test/basic_test.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require '../llvmruby'
+
+include LLVM
+
+class BasicTests < Test::Unit::TestCase
+  def test_simple
+    f = Function.new
+    block = f.create_block
+    b = block.builder
+    x = Value.get_constant(23)
+    y = Value.get_constant(42)
+    tmp = b.create_add(x, y)
+    b.create_return(tmp)
+    f.compile
+    assert_equal(65, f.call(0))
+  end
+end
