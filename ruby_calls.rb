c3e2d494c7265a671752fc79329ba55330b524a1
Tom Bagby <tombagby@gmail.com>
2008-08-24 12:41:49 -0700

    more complex ruby array manipulation examples

--- ruby_calls.rb
+++ ruby_calls.rb
@@ -35,5 +35,74 @@ b.aset(ary, idx_y, x)
 b.create_return(ary)
 swap = f
 
-ret = ExecutionEngine.run_function(swap, [1,2,3,4,23])
-puts "returned: #{ret.inspect}"
+# Add 1 to every array element (in place)
+type = Type.function(Type::Int64Ty, [Type::Int64Ty])
+f = m.get_or_insert_function('add1', type)
+ary = f.arguments.first
+
+entry_block = f.create_block
+loop_block = f.create_block
+exit_block = f.create_block
+
+b = entry_block.builder
+len = b.alen(ary)
+cmp = b.create_icmpeq(0.llvm, len)
+b.create_cond_br(cmp, exit_block, loop_block)
+
+b = loop_block.builder
+idx = b.create_phi(Type::Int64Ty)
+idx.add_incoming(0.llvm, entry_block)
+next_idx = b.add(1.llvm, idx)
+idx.add_incoming(next_idx, loop_block)
+v = b.fix2int(b.aref(ary, idx))
+v1 = b.add(1.llvm, v)
+b.aset(ary, idx, b.num2fix(v1))
+cmp = b.create_icmpult(next_idx, len)
+b.create_cond_br(cmp, loop_block, exit_block)
+
+b = exit_block.builder
+b.create_return(ary)
+add1 = f
+
+ret = ExecutionEngine.run_function(add1, [1,2,3,4,23])
+puts "add1: #{ret.inspect}"
+
+# Add 1 to every array element (in place)
+type = Type.function(Type::Int64Ty, [Type::Int64Ty])
+f = m.get_or_insert_function('reverse', type)
+ary = f.arguments.first
+
+entry_block = f.create_block
+loop_block = f.create_block
+exit_block = f.create_block
+
+b = entry_block.builder
+len = b.alen(ary)
+half_len = b.udiv(len, 2.llvm)
+last_idx = b.sub(len, 1.llvm)
+cmp = b.create_icmpeq(0.llvm, len)
+b.create_cond_br(cmp, exit_block, loop_block)
+
+b = loop_block.builder
+idx = b.create_phi(Type::Int64Ty)
+idx.add_incoming(0.llvm, entry_block)
+next_idx = b.add(1.llvm, idx)
+idx.add_incoming(next_idx, loop_block)
+
+# do the swap
+idx_x = idx
+idx_y = b.sub(last_idx, idx)
+x = b.aref(ary, idx_x)
+y = b.aref(ary, idx_y)
+b.aset(ary, idx_x, y)
+b.aset(ary, idx_y, x)
+
+cmp = b.create_icmpult(next_idx, half_len)
+b.create_cond_br(cmp, loop_block, exit_block)
+
+b = exit_block.builder
+b.create_return(ary)
+reverse = f
+
+ret = ExecutionEngine.run_function(reverse, [1,2,3,4,23])
+puts "reverse: #{ret.inspect}"
